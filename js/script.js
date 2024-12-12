137d352497e9f110dfe8ad7b619d4671b49f9a4a
Paulina Partyka <paulinapartyka2@gmail.com>
2024-12-12 09:08:24 +0100

    add global handlebars

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* Global Handlebars */
+
 const templates = {
   articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
   tagLink:Handlebars.compile(document.querySelector('#template-tag-link').innerHTML),
